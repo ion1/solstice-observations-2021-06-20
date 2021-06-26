4c49a515283890bae9646c7328078ba766fd5be2
Johan Kiviniemi <devel@johan.kiviniemi.name>
2021-06-26 09:01:27 +0300

    Add site metadata

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,13 +4,36 @@
 </script>
 
 <script type="ts">
+  import SvelteSeo from "svelte-seo";
+
   import Diagram from "$lib/Diagram.svelte";
   import Info from "$lib/Info.svelte";
+
+  const title = "A Visualization of the 2021 Solstice Observations";
+  const description =
+    "An interactive visualization of the 2021 solstice sun angle observations organized by I Can Science That.";
+  const canonicalUrl = "https://solstice2021.heh.fi/";
+  const imageUrl = `${canonicalUrl}thumbnail.png`;
 </script>
 
-<svelte:head>
-  <title>A Visualization of the 2021 Solstice Observations</title>
-</svelte:head>
+<SvelteSeo
+  {title}
+  {description}
+  canonical={canonicalUrl}
+  openGraph={{
+    type: "website",
+    title,
+    description,
+    images: [{ url: imageUrl }],
+    url: canonicalUrl,
+  }}
+  twitter={{
+    site: "@hajotus",
+    title,
+    description,
+    image: imageUrl,
+  }}
+/>
 
 <Diagram />
 
